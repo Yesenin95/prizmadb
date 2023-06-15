0b2ac90a701e6e3f464004585e68ee4fe66a270d
Николай <nikolatrofik@gmail.com>
2023-06-15 14:19:51 +0300

    prismadb

---
+++ experiments/1_create.mjs
@@ -0,0 +1,9 @@
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+const newUser = await prisma.user.create({
+   data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+   },
+})
